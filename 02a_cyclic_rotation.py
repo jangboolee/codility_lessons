bbbd2c7bac7c182ef3af03cb19435b6af8a82afa
Jangboo Lee <github@jangboolee.com>
2021-08-20 18:51:00 +0000

    Create 02a_cyclic_rotation.py

---
+++ 02a_cyclic_rotation.py
@@ -0,0 +1,25 @@
+def solution(A, K):
+
+    # Create a blank list with 0 as default values with the same length as A
+    rotated = [0] * len(A)
+
+    # Return the same list if there is no or one element in the list
+    if len(A) < 1:
+        return A
+
+    # Adjust K if K is longer than A
+    if K > len(A):
+        K  = K % len(A)
+
+    # Loop through each index for each element in A
+    for i in range(len(A)):
+        
+        # If the only needs to be moved right
+        if i + K < len(A):
+            rotated[i+K] = A[i]
+
+        # If the item needs to be moved left and then right
+        else:
+            rotated[i-len(A)+K] = A[i]
+
+    return rotated
